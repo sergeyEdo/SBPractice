ec155de6cf2dfc1448c4b24afada2d5ae9353f11
sergeyEdo <serega.edoshin@gmail.com>
2025-07-01 18:49:29 +0300

    unit5_3_fix

--- unit_5_3/src/main/java/util/Util.java
+++ unit_5_3/src/main/java/util/Util.java
@@ -8,12 +8,12 @@ public class Util {
     private Util() {}
 
     public static <V,K> Map<V,K> reverseMap(Map<K,V> map){
-        Map<V,K> reversed_map = new HashMap<>();
+        Map<V,K> reversedMap = new HashMap<>();
         for(Map.Entry<K,V> entry : map.entrySet()){
             K entryKey = entry.getKey();
             V entryVal = entry.getValue();
-            reversed_map.put(entryVal, entryKey);
+            reversedMap.put(entryVal, entryKey);
         }
-        return reversed_map;
+        return reversedMap;
     }
 }
